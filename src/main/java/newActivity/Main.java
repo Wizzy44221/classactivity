95cbf554c4bb1bc82aa544024874a773e5854155
Wisdom Onu Ugorji <wizzypatrick777@gmail.com>
2024-10-14 23:37:49 +0100

    adding files

--- src/main/java/newActivity/Main.java
+++ src/main/java/newActivity/Main.java
@@ -4,7 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ClassActivity newActivity = new ClassActivity();
+//        ClassActivity newActivity = new ClassActivity();
+int a = 4;
+        int b =5;
 
+        int sum = a + b;
+
+        System.out.println(sum);
     }
 }
